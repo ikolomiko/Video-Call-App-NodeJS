7638f25eba11ccbfe09df398312f8116f05dd5ea
İlker AVCI <ilk-1er@hotmail.com>
2020-03-26 01:28:21 +0300

    Still trying

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 
+app.get('/.well-known/acme-challenge/DlbxA9pOv9wpmGohOlKLqdQKI73X1_FhX1m5MUqRMvY', 
+(req,res) => {
+    res.send('DlbxA9pOv9wpmGohOlKLqdQKI73X1_FhX1m5MUqRMvY.vy9Dgj_rJTvLQpbq090ysEtFlZqH6T3y8r2mCFSy60s');
+});
+
 
 io.of('/stream').on('connection', stream);
 
